f429fd3720a83f2bb93920aff677cf159addb331
Victor <victoryang2005@outlook.com>
2023-07-22 14:21:22 -0700

    Initial Commit

---
+++ ios Network Calculator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ios Network Calculator
+//
+//  Created by Victor Yang on 2023-07-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios Network Calculator/ios_Network_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_Network_CalculatorApp.swift
+//  ios Network Calculator
+//
+//  Created by Victor Yang on 2023-07-22.
+//
+
+import SwiftUI
+
+@main
+struct ios_Network_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
